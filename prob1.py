14c1f9ced0a44772394a31d3427b8bc95499fd73
roshinipushpika <roshinipushpikachowdary@gmail.com>
2026-01-06 22:06:25 +0530

    Updated version code

--- prob1.py
+++ prob1.py
@@ -1,1 +1,27 @@
-print("Hi")
+print("Hi")  
+import random
+
+# List of choices
+choices = ["rock", "paper", "scissors"]
+
+# Computer makes a random choice
+computer = random.choice(choices)
+
+# User input
+user = input("Enter rock, paper, or scissors: ").lower()
+
+print("Computer chose:", computer)
+
+# Game logic
+if user == computer:
+    print("It's a tie!")
+elif user == "rock" and computer == "scissors":
+    print("You win!")
+elif user == "paper" and computer == "rock":
+    print("You win!")
+elif user == "scissors" and computer == "paper":
+    print("You win!")
+elif user in choices:
+    print("Computer wins!")
+else:
+    print("Invalid input!")
